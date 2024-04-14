ddba9813e54d506f600325316fb75f0e9fd253da
mrmarfi <filippo.erbi@mail.polimi.it>
2024-04-14 10:52:18 +0200

    added string message in WrongPlayException

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/WrongPlayException.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/WrongPlayException.java
@@ -2,13 +2,17 @@ package it.polimi.ingsw.model;
 
 public class WrongPlayException extends Throwable{
     Player player;
-    int x;
-    int y;
+    String message;
     Card card;
     WrongPlayException(Player player,int x,int y, Card card){
         this.player=player;
-        this.x=x;
-        this.y=y;
         this.card=card;
+        if (x==-1 && y==-1){
+            message= "Error: the card cant be found in the hand of the player";
+        } else if (x==-2 && y==-2) {
+            message= "Error: the card doesnt overlap any corner";
+        } else if (x==-3 && y==-3) {
+            message="Error: the player doesnt have enough resources to play that card";
+        } else message= "Error: the card cant be played in the position: "+x+","+y;
     }
 }
