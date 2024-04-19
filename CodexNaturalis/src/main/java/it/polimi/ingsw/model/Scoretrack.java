72cb41c535d4c1fee2e243793ce18163e7e988b0
gianluspng <gianlusuperiori@gmail.com>
2024-04-19 11:40:30 +0200

    Add javadoc for Scoretrack class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Scoretrack.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Scoretrack.java
@@ -1,17 +1,28 @@
 package it.polimi.ingsw.model;
 
+/**
+ *  Class containing the score track on which the tokens will be moving
+ */
 public class Scoretrack {
-
+    /**
+     * array containing all the token positions for the current game
+     */
     private int[] tokenPos;
 
+    /**
+     * Constructor: initializes array representing all the 30 positions of the score track (from 0 to 29)
+     */
     public Scoretrack() {
         tokenPos = new int[30];
     }
 
+    /**
+     * getter for the token positions array
+     * @return token positions array
+     */
     public int[] getTokenPos() {
         return tokenPos;
     }
 
-    //Non ho messo triggerendgame perché era un giro troppo lungo. invece update() della classe currentserousces
-    //chiamerà endgame() di Game.
+
 }
