2b1f4406a216c0f173a39dfc0eb670e3875b318a
Alessandro-Lorenzini <161436728+Alessandro-Lorenzini@users.noreply.github.com>
2024-04-20 10:37:02 +0200

    JSON parse added for all the cards

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/ObjectiveCard1.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/ObjectiveCard1.java
@@ -7,13 +7,13 @@ public class ObjectiveCard1 extends ObjectiveCard{
     public int[] req1= new int[3];
     public Color[] Color= new Color[4];
 
+    public ObjectiveCard1(int ID) {
+        super(ID);
+    }
+
     //TODO aggiungere costruttore
 
     public ObjectiveCard getCard(){
         return this;
-    }
-
-    public int getPoints(){
-        return points;
     }
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/StartingCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/StartingCard.java
@@ -1,12 +1,32 @@
 package it.polimi.ingsw.model;
 
+import com.google.gson.Gson;
 import it.polimi.ingsw.model.Player;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 public class StartingCard extends Card{
-    public Resource[] resource = new Resource[7];
+
+    public Resource[] resource = new Resource[3];
 
     public StartingCard(int ID){
         super(ID);
+
+        isFacedown = false;
+
+        Gson gson = new Gson();
+
+        try (FileReader reader = new FileReader("src/main/resources/assets/cards/starting_card.json")) {
+
+            ResourceCard[] rCards = gson.fromJson(reader, ResourceCard[].class);
+
+        } catch (IOException e) {
+            // This is only for debugging: it'll be removed later.
+            System.out.println("FILE non trovato!");
+
+            // TODO: add the catch action
+        }
     }
     public void flip(){
         isFacedown = !isFacedown;
