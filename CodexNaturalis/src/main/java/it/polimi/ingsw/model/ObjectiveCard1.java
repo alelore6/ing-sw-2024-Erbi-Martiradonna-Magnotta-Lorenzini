9ec24830001e0677b5df5ee494aeec399665911b
Alessandro-Lorenzini <161436728+Alessandro-Lorenzini@users.noreply.github.com>
2024-04-21 14:17:55 +0200

    JSON parse COMPLETED

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/ObjectiveCard1.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/ObjectiveCard1.java
@@ -3,9 +3,10 @@ package it.polimi.ingsw.model;
 
 
 public class ObjectiveCard1 extends ObjectiveCard{
-
-    public int[] req1= new int[3];
-    public Color[] Color= new Color[4];
+    // req and color have a 1 to 1 mapping: it means that req[i] is referred to color[i].
+    // They also have length 3.
+    private int[] req;
+    private Color[] color;
 
     public ObjectiveCard1(int ID) {
         super(ID);
