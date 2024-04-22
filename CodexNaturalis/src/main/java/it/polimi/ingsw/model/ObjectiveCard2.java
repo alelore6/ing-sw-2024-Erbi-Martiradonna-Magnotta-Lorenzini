12ec3f940d80015e9a24c19fe0a173bcacfb6672
MicheleMagnotta <161478203+MicheleMagnotta@users.noreply.github.com>
2024-04-22 14:22:17 +0200

    Created Constructor and added shuffle logic

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/ObjectiveCard2.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/ObjectiveCard2.java
@@ -1,17 +1,31 @@
 package it.polimi.ingsw.model;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.gson.Gson;
 
 public class ObjectiveCard2 extends ObjectiveCard {
-    //TODO aggiungere costruttore
-    Map <Resource,Integer> ObjectiveCard2;
-    ObjectiveCard2(){
-        ObjectiveCard2 =new HashMap<Resource,Integer>();
-        //TODO da sistemare
-        for (Resource k: Resource.values()){
-            ObjectiveCard2.put(k,0);
+    private Resource points;
+    private Map<Resource, Integer> objectivecard2Map;
+
+    public ObjectiveCard2(int ID) {
+        super(ID);
+        objectivecard2Map = new HashMap<>();
+        for (Resource k : Resource.values()) {
+            objectivecard2Map.put(k, 0);
         }
+
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader("src/main/resources/assets/cards/objective_card.json")) {
+            ObjCard[] oCards = gson.fromJson(reader, ObjCard[].class); //
+        } catch (IOException e) {
+           System.out.println("Error, not found.");
+        }
+    }
+    public Resource getpoints() {
+        return points;
     }
 }
