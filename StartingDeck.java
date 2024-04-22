985ca80f91a8393e7eb4a6eb8749e3bb1ef67d0f
MicheleMagnotta <161478203+MicheleMagnotta@users.noreply.github.com>
2024-04-22 14:17:04 +0200

    Shuffle logic and fixing errors

---
+++ StartingDeck.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.model;
+
+import java.util.Collections;
+
+public class StartingDeck {
+
+    int NCards = 6;
+    private StartingCard[] startcards;
+
+    public StartingDeck() {
+        startcards = new StartingCard[6];
+        for (int i = 0; i < 6; i++) {
+            startcards[i] = new StartingCard(6 + i);
+        }
+        public StartingCard draw () throws isEmptyException {
+            if (NCards == 0) {
+                throw new isEmptyException(this);
+            } else {
+                StartingCard drawnCard = startcards[--NCards];
+                return drawnCard;
+            } //Not removing from array,
+        }
+    }
+        public void shuffle () {
+            Collections.shuffle(startcards);
+        }
+
+    }
+}
