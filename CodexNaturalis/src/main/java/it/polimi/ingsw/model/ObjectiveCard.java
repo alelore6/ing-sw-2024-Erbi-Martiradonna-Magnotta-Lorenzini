c30f37f36d74ec1c7014cbeca14f11b7b8362d82
MicheleMagnotta <161478203+MicheleMagnotta@users.noreply.github.com>
2024-04-22 14:37:53 +0200

    insert JS in specific class to keep smarter this

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/ObjectiveCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/ObjectiveCard.java
@@ -1,30 +1,12 @@
 package it.polimi.ingsw.model;
 
-import com.google.gson.Gson;
-
-import java.io.FileReader;
-import java.io.IOException;
-
 public abstract class ObjectiveCard {
 
-    protected int ID;
-    protected int points;
+    private int ID;
+    public int points;
 
-    ObjectiveCard(int ID) {
+    public ObjectiveCard(int ID) {
         this.ID = ID;
-
-        Gson gson = new Gson();
-
-        try (FileReader reader = new FileReader("src/main/resources/assets/cards/objective_card.json")) {
-
-            ResourceCard[] rCards = gson.fromJson(reader, ResourceCard[].class);
-
-        } catch (IOException e) {
-            // This is only for debugging: it'll be removed later.
-            System.out.println("FILE non trovato!");
-
-            // TODO: add the catch action
-        }
     }
     public abstract ObjectiveCard getCard();
     public int getpoints(){
