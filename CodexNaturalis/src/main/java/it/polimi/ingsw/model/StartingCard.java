384477332f76cb42734ef25c80b584647dfdc54b
MicheleMagnotta <161478203+MicheleMagnotta@users.noreply.github.com>
2024-04-22 14:31:24 +0200

    JSON and fixed errors

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/StartingCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/StartingCard.java
@@ -1,31 +1,19 @@
 package it.polimi.ingsw.model;
 
-import com.google.gson.Gson;
 import it.polimi.ingsw.model.Player;
 
-import java.io.FileReader;
-import java.io.IOException;
 
+import com.google.gson.Gson;
 public class StartingCard extends Card{
-
     public Resource[] resource = new Resource[3];
 
     public StartingCard(int ID){
         super(ID);
-
-        isFacedown = false;
-
         Gson gson = new Gson();
-
         try (FileReader reader = new FileReader("src/main/resources/assets/cards/starting_card.json")) {
-
-            ResourceCard[] rCards = gson.fromJson(reader, ResourceCard[].class);
-
+            StartingCard[] sCards = gson.fromJson(reader, StartingCard[].class);
         } catch (IOException e) {
-            // This is only for debugging: it'll be removed later.
-            System.out.println("FILE non trovato!");
-
-            // TODO: add the catch action
+            System.out.println("Error, not found.");
         }
     }
     public void flip(){
