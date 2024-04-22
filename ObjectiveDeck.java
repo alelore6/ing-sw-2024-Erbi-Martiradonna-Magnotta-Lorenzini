63b3e274f4b0ead3abd4a417cb60ff04669dd2cf
MicheleMagnotta <161478203+MicheleMagnotta@users.noreply.github.com>
2024-04-22 14:15:14 +0200

    Shuffle logic and deck  base requirements

---
+++ ObjectiveDeck.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.model;
+
+import java.util.Collections;
+
+public class ObjectiveDeck {
+    int NCards = 16;
+    private ObjectiveCard[] objcards;
+
+    public ObjectiveDeck() {
+        objcards = new ObjectiveCard[16];
+        for (int i = 0; i < 16; i++) {
+            objcards[i] = new ObjectiveCard(16 + i);
+        }
+        public ObjectiveCard draw () throws isEmptyException {
+            if (NCards == 0) {
+                throw new isEmptyException(this);
+            } else {
+                ObjectiveCard drawnCard = objcards[--NCards];
+                return drawnCard;
+            }
+        }
+        public ObjectiveCard draw () {
+            return objcards;
+        }
+        public void shuffle () {
+            Collections.shuffle(objcards);
+        }
+    }
+}
