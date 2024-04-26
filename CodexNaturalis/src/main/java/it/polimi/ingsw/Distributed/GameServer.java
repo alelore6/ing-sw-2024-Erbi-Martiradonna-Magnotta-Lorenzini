ce5bd33f4ae82ece86190e76ee580393d131fdcd
gianluspng <gianlusuperiori@gmail.com>
2024-04-26 15:16:37 +0200

    add constructor for GameServer

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Distributed/GameServer.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Distributed/GameServer.java
@@ -2,13 +2,31 @@ package it.polimi.ingsw.Distributed;
 
 import it.polimi.ingsw.controller.Controller;
 
+import java.rmi.RemoteException;
+import java.rmi.server.RMIClientSocketFactory;
+import java.rmi.server.RMIServerSocketFactory;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GameServer implements Server{
+public class GameServer extends UnicastRemoteObject implements Server{
     public Controller controller = new Controller(this);
     private static final List<Client> clientList = new ArrayList<>();
     private static int numClient = 0;
+
+    //server constructor with the default rmi port
+    protected GameServer() throws RemoteException {
+        super();
+        //need to bind the controller to the model, something like controller.setgame(model)
+    }
+
+    //server implementation with a certain RMI port
+    protected GameServer(int port) throws RemoteException {
+        super(port);
+        //need to bind the controller to the model, something like controller.setgame(model)
+    }
+
+
 
     public boolean addClient(Client client){
         // controllo nickname
