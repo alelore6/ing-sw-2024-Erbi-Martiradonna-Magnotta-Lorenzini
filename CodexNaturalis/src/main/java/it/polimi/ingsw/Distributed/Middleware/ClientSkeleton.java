99deda27e7f3e9a81cc6c90e33da1daa046ac736
gianluspng <gianlusuperiori@gmail.com>
2024-04-26 19:19:34 +0200

    Begin clientskeleton implementation

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Distributed/Middleware/ClientSkeleton.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Distributed/Middleware/ClientSkeleton.java
@@ -1,4 +1,23 @@
 package it.polimi.ingsw.Distributed.Middleware;
 
-public class ClientSkeleton {
+import it.polimi.ingsw.Distributed.Client;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.rmi.RemoteException;
+
+public class ClientSkeleton implements Client{
+
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+
+    public ClientSkeleton(Socket socket)throws RemoteException {
+        //TODO implement
+    }
+
+    @Override
+    public void update(PreviousView v, Event e) throws RemoteException {
+
+    }
 }
