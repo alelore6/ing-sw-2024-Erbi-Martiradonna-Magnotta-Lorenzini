44810c10f0d4fdf6d62b453fc5fd6ad110434772
MicheleMagnotta <161478203+MicheleMagnotta@users.noreply.github.com>
2024-05-02 18:59:29 +0200

    Started creating test of the deck

--- CodexNaturalis/src/test/java/it/polimi/ingsw/model/DeckTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/model/DeckTest.java
@@ -1,11 +1,20 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.Exceptions.isEmptyException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
+import static org.mockito.Mockito.*;
+// dovrei creare il file Maven pom.xml per inserire le seguenti dipendenze
+// <dependency>
+//    <groupId>org.mockito</groupId>
+//    <artifactId>mockito-core</artifactId>
+//    <version>3.12.4</version>
+//    <scope>test</scope>
+//</dependency>
+// per poter importare Mockito e sfruttare i mock.
 public class DeckTest {
 
     @Before
@@ -18,6 +27,14 @@ public class DeckTest {
 
     @Test
     public void draw() {
+        Card testcard = mock(testcard.Card);
+        Deck deck = new Deck()
+        try{
+            Card drawnCard = deck.draw();
+            assertEquals(testcard, drawnCard);
+        } catch (isEmptyException e){
+            fail("Error during draw.");
+        }
     }
 
     @Test
