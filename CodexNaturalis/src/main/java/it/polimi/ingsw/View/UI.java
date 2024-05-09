d0c46bb1bdaeaecfa636ceab63ff8d622de3ae10
Alessandro-Lorenzini <161436728+Alessandro-Lorenzini@users.noreply.github.com>
2024-05-09 17:33:38 +0200

    nickname fixes

--- CodexNaturalis/src/main/java/it/polimi/ingsw/View/UI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/View/UI.java
@@ -2,24 +2,26 @@ package it.polimi.ingsw.View;
 
 import it.polimi.ingsw.Distributed.ClientImpl;
 import it.polimi.ingsw.Listeners.ViewControllerListener;
+import it.polimi.ingsw.model.Card;
 
 import java.util.ArrayList;
 
 public abstract class UI implements View{
 
-    public final String nickname;
-
+    protected final ClientImpl client;
     protected final ViewControllerListener listener;
 
     // WATCH OUT! This constructor allows the creation of a TUI w/o having created the actual player
     // The nickname here is not the player's but a test one.
     public UI(String nickname) {
-        this.nickname = nickname;
+        this.client = null;
         this.listener = null;
     }
 
+    protected abstract void printCard(Card card);
+
     public UI(ClientImpl client) {
-        this.nickname = client.getNickname();
+        this.client = client;
         this.listener = new ViewControllerListener(client);
     }
 }
