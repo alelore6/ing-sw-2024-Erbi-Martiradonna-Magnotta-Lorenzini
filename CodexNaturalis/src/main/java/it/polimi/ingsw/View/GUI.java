1e0cf11e0465205538b18a47dca3de01f8b00b59
mrmarfi <filippo.erbi@mail.polimi.it>
2024-05-17 16:18:16 +0200

    starting gui implementation

--- CodexNaturalis/src/main/java/it/polimi/ingsw/View/GUI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/View/GUI.java
@@ -1,12 +1,14 @@
 package it.polimi.ingsw.View;
 
 import it.polimi.ingsw.Distributed.ClientImpl;
-import it.polimi.ingsw.Events.GenericEvent;
+import it.polimi.ingsw.Events.*;
 import it.polimi.ingsw.Graphical.MainFrame;
 import it.polimi.ingsw.Listeners.ViewControllerListener;
 import it.polimi.ingsw.Model.Card;
 
 import javax.swing.*;
+
+import java.util.LinkedList;
 
 import static java.lang.String.valueOf;
 
@@ -15,7 +17,15 @@ public class GUI extends UI{
     private static MainFrame f;
     public GUI(ClientImpl client) {
         super(client);
+        inputMessages = new LinkedList<>();
+        f = new MainFrame("CodexNaturalis");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(1280,720);
+        f.setVisible(true);
+        //aggiungere sfondo
+        printCard(102,false,100,100,0.6);
     }
+
 
     @Override
     protected void printCard(Card card) {
@@ -69,21 +79,64 @@ public class GUI extends UI{
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
 
-                new Thread(){
-                    @Override
-                    public void run() {
-                        f = new MainFrame("CodexNaturalis");
-                        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                        f.setSize(1280,720);
-                        f.setVisible(true);
-                        printCard(102,false,100,100,0.6);
+                while(true){
+                    if(inputMessages.isEmpty())   continue;
+                    GenericEvent ev = inputMessages.poll();
+                    // Ignore all other player's events
+                    if(!ev.nickname.equals(client.getNickname())) continue;
+                    //debug
+                    printOut(ev.msgOutput());
+                    int n;
+                    GenericEvent newEvent;
+                    switch(ev){
+                        case NumPlayersRequest e :
+                            JDialog dialog=new JDialog();
+                            dialog.setDefaultCloseOperation(JDialog.EXIT_ON_CLOSE);
+                            dialog.setSize(640,360);
+                            dialog.setVisible(true);
+                            dialog.add(new JLabel(ev.msgOutput()));
+                            ButtonGroup group = new ButtonGroup();
+                            group.add(new JRadioButton("2"));
+                            group.add(new JRadioButton("3"));
+                            group.add(new JRadioButton("4", true));
+                            //dialog.add(group);
+                            JButton confirm=new JButton("Confirm");
+                            //confirm.setActionCommand();
+                            //confirm.addActionListener();
+                            dialog.add(confirm);
+                            f.add(dialog);
+                            f.repaint();
+                            //newEvent = new NumPlayersResponse( , client.getNickname());
+                            //notifyListener(listener, newEvent);
+                            break;
 
-//                        while(true){
-//                            //gestione eventi continuo come nella TUI
-//                        }
-                    }
-                }.start();
+                        case ChooseObjectiveRequest e :
+                            //mostrare dati evento aggionando il frame
+                            //quando decisione presa mando evento risposta
+                            //newEvent = new ChooseObjectiveResponse( , client.getNickname());
+                            //notifyListener(listener, newEvent);
+                            //printOut(newEvent.msgOutput());
+                            break;
 
+                        case DrawCardRequest e :
+                            break;
+
+                        case PlayCardRequest e :
+                            break;
+
+                        case SetTokenColorRequest e :
+                            break;
+
+                        case JoinLobby e :
+                            break;
+
+                        case PlaceStartingCard e :
+                            break;
+
+                        default:
+                            throw new IllegalStateException("Unexpected value: " + ev.toString());
+                                }
+                            }
             }
         });
     }
